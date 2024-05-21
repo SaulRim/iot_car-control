c3d90affd50a8e1c1b629058c1eeb51de7e031d5
Saul Ricaño Monroy <163690379+SaulRim@users.noreply.github.com>
2024-05-21 11:04:34 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -34,7 +34,7 @@ function callApiRequest(status) {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "https://3.128.188.72/iot-car-control/back-end/apis/setRegistro.php?valorEstatus="+status
+      "https://3.17.149.237/iot-car-control/back-end/apis/setRegistro.php?valorEstatus="+status
     )
     .then(function (response) {
       // manejar respuesta exitosa
